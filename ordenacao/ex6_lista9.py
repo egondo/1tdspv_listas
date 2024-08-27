7caf1f7ace692f6a9821f5d462d7031caaba61a7
Eduardo Gondo <egondo@gmail.com>
2024-08-27 19:29:34 -0300

    exercicio de ordenação

--- ordenacao/ex6_lista9.py
+++ ordenacao/ex6_lista9.py
@@ -13,4 +13,4 @@ def insertion_sort(lista, campo):
 pessoas = [('Andre', 18), ('Thiago', 23), ('Soraia', 22), ('Mateus', 16), ('Bernardo', 23), ('Cibele', 17), ('Oscar', 18), ('Thiago', 15), ('Cintia', 36), ('Daniela', 15), ('Mario', 38), ('Soraia', 16), ('Lais', 16), ('Cintia', 20), ('Mario', 22)]
 insertion_sort(pessoas, 1)
 insertion_sort(pessoas, 0)
-print(pessoas)
+#print(pessoas)
