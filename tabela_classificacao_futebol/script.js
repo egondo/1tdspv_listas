785da6711f46be2057c6d91c427360887cb0baea
Eduardo Gondo <egondo@gmail.com>
2024-10-15 20:56:09 -0300

    Exemplo de chamada com method post

--- tabela_classificacao_futebol/script.js
+++ tabela_classificacao_futebol/script.js
@@ -14,8 +14,27 @@ const consulta = () => {
         })
 }
 
+const cadastra = () => {
+    cmp_visi = document.querySelector('[name="visi"]')
+    cmp_gv = document.querySelector('[name="gv"]')
+    cmp_casa = document.querySelector('[name="casa"]')
+    cmp_gc = document.querySelector('[name="gc"]')
+
+    dado = {casa: cmp_casa.value, visi: cmp_visi.value,
+        gc: parseInt(cmp_gc.value), gv: parseInt(cmp_gv.value),
+        rodada: 3}
+    fetch("http://127.0.0.1:5000/partidas", {
+        method: "POST",
+        body: JSON.stringify(dado),
+        headers: {"Content-Type": "application/json"}
+    })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+}
+botao_cadastra = document.querySelector('[name="cadastra"]')
+botao_cadastra.addEventListener("click", cadastra)
+
 botao_times = document.querySelector('[name="times"]')
 botao_times.addEventListener("click", consulta)
 
-botao_cadastra = document.querySelector('[name="cadastra"]')
 
