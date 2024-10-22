b088c11ecb0a77841fdfced768a26bc642233a8f
Eduardo Gondo <egondo@gmail.com>
2024-10-22 19:42:29 -0300

    Gravacao da enquete

--- projeto_enquete/banco.py
+++ projeto_enquete/banco.py
@@ -5,7 +5,7 @@ def get_conexao():
                         dsn="oracle.fiap.com.br/orcl")
 
 def insere_enquete(enquete: dict):
-    sql = "insert into tbv_enquete(nome, data) values(:nome, sysdate) returning id into :id"
+    sql = "insert into tbv_enquete(nome) values(:nome) returning id into :id"
     with get_conexao() as con:
         with con.cursor() as cur:
             novo_id = cur.var(oracledb.NUMBER)
@@ -16,7 +16,39 @@ def insere_enquete(enquete: dict):
         con.commit()
 
 
-def insere_perguntas(perguntas: list):
+def insere_perguntas(perguntas: list, id_enquete: int):
+    sql = "insert into tbv_pergunta(numero, texto, tipo, id_enquete) values(:numero, :texto, :tipo, :id_enquete) returning id into :id"
+    sql_item = "insert into tbv_item(numero, descricao, id_pergunta) values(:numero, :descricao, :id_pergunta)"
+
+    with get_conexao() as con:
+        with con.cursor() as cur:
+            for questao in perguntas:
+
+                questao['id_enquete'] = id_enquete
+
+                if questao['tipo'] != 'aberta':
+                    itens = questao.pop('itens')
+                else:
+                    itens = None
+
+                novo_id = cur.var(oracledb.NUMBER)
+                questao['id'] = novo_id
+                cur.execute(sql, questao)
+                questao['id'] = novo_id.getvalue()[0]
+                
+                if itens != None:
+                    for item in itens:
+                        item['id_pergunta'] = questao['id']
+                        cur.execute(sql_item, item)
+        con.commit()
+
+
+
+
+
+
+
+
 
 
 
--- projeto_enquete/negocio.py
+++ projeto_enquete/negocio.py
@@ -20,7 +20,29 @@ import banco
     ]
 }'''
 def cadastra_enquete(objeto: dict):
-    enq = {"nome": objeto.nome}
+    enq = {"nome": objeto['nome']}
     banco.insere_enquete(enq)
-    banco.insere_perguntas(objeto.perguntas, enq.id)
-    
+    banco.insere_perguntas(objeto['perguntas'], enq['id'])
+
+
+if __name__ == "__main__":
+    e = {
+    "nome": "Pesquisa eleitoral",
+    "perguntas": [
+        {"numero": 1, "texto": "Qual o seu nome?", "tipo": "aberta"},
+        {"numero": 2, "texto": "Qual a sua idade?", "tipo": "aberta"},
+        {"numero": 3, "texto": "Gênero?", "tipo": "escolha única", 
+            "itens": [{"numero": 1, "descricao": "masculino"},
+                      {"numero": 2, "descricao": "feminino"}
+                    ]
+        },
+        {"numero": 4, "texto": "Há quantos anos você vota?", "tipo": "aberta"},
+        {"numero": 5, "texto": "Em quem você vai votar?", "tipo": "escolha única",
+            "itens": [
+                {"numero": 1, "descricao": "Guilherme Boulos"},
+                {"numero": 2, "descricao": "Ricardo Nunes"}
+            ]
+        }
+    ]
+    }
+    cadastra_enquete(e)
